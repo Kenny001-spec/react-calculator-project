64a6a4bf436a5a0a7cefec6d681a136bef634d41
kehinde01 <abubakarkehinde88@gmail.com>
2022-08-20 12:54:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+
+import { clear } from '@testing-library/user-event/dist/clear';
+import React, { useState } from 'react'
+import './style.css';
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const handClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+
+  const AC = () => {
+    setResult("");
+  }
+
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  const Addition = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error")
+    }
+
+  }
+
+
+  return (
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button className='highlight' onClick={AC} id="ac">AC</button>
+          <button className='highlight' onClick={backspace} id="backspace">DEL</button>
+          <button className='highlight' name="/" onClick={handClick}>&divide;</button>
+          <button name="7" onClick={handClick}>7</button>
+          <button name="8" onClick={handClick}>8</button>
+          <button name="9" onClick={handClick}>9</button>
+          <button className='highlight' name="*" onClick={handClick}>&times;</button>
+          <button name="4" onClick={handClick}>4</button>
+          <button name="5" onClick={handClick}>5</button>
+          <button name="6" onClick={handClick}>6</button>
+          <button className='highlight' name="-" onClick={handClick}>&ndash;</button>
+          <button name="1" onClick={handClick}>1</button>
+          <button name="2" onClick={handClick}>2</button>
+          <button name="3" onClick={handClick}>3</button>
+          <button className='highlight' name="+" onClick={handClick}>+</button>
+          <button name="0" onClick={handClick}>0</button>
+          <button name="." onClick={handClick}>.</button>
+          <button className='highlight' onClick={Addition} id="result">=</button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
+
